95a42e3a47f028c243d1b63d592c8aafc5e3adad
Apple1995-glitch <Apple1995@centrum.cz>
2026-08-30 15:56:30 +0200

    Update ⁠sw.js

--- ⁠sw.js
+++ ⁠sw.js
@@ -1,3 +1,23 @@
+const CACHE_NAME = 'taxi-martin-v1';
+const urlsToCache = [
+  '/Taxi-Martin-/',
+  '/Taxi-Martin-/index.html',
+  '/Taxi-Martin-/manifest.json',
+  '/Taxi-Martin-/IMG_2858.jpeg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
